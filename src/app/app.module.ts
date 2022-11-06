1ae91002405914c1749566bd12c047c5dd12d4ea
restomatic <restomatic@users.noreply.github.com>
2022-11-06 15:29:41 +0100

    Création de la structure de l'applcation en différent modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppAlertesModule } from './modules/alertes/app.module';
+import { AppAnalyseModule } from './modules/analyse/app.module';
+import { AppAuthenModule } from './modules/authen/app.module';
+import { AppAuthoModule } from './modules/autho/app.module';
+import { AppBudgetModule } from './modules/budget/app.module';
+import { AppFacturesModule } from './modules/factures/app.module';
+import { AppStockModule } from './modules/stock/app.module';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from 'src/environments/environment';
 
@@ -11,6 +18,14 @@ import { environment } from 'src/environments/environment';
   ],
   imports: [
     BrowserModule,
+    //import des différent module propre à chacune des pages de l'application
+    AppAlertesModule,
+    AppAnalyseModule,
+    AppAuthenModule,
+    AppAuthoModule,
+    AppBudgetModule,
+    AppFacturesModule,
+    AppStockModule,
     // permet d'initialiser les services firebase à utiliser une fois dans
     //le app.module les objet de classe FirebaseApp contiendrons alors la configuration pour l'api firebase présent dans l'environnement
     provideFirebaseApp(() => initializeApp(environment.firebase)),
