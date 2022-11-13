d2015c2ac64a34980e2b1b092c7c5bf40b7d88bc
Amin Akkouche <aminakk42@gmail.com>
2022-11-13 14:51:00 +0100

    navbarV2

--- src/app/modules/acceuil/app.module.ts
+++ src/app/modules/acceuil/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentAcceuil } from './app.component.acceuil/app.component.acceuil';
 import { AppModuleRoutingModule } from './app.module-routing';
-import { NavbarVitrineComponent } from './app.component.acceuil/navbar-vitrine/navbar-vitrine.component'
+import { NavbarVitrineComponent } from './app.component.acceuil/navbar-vitrine/navbar-vitrine.component';
+import { ConnectionComponent } from './app.component.acceuil/connection/connection.component'
 
 @NgModule({
-  declarations: [ComponentAcceuil, NavbarVitrineComponent],
+  declarations: [ComponentAcceuil, NavbarVitrineComponent, ConnectionComponent],
   imports: [CommonModule, AppModuleRoutingModule],
   exports: [],
   providers: [],
