d9f4e22fbc58b3ba3ab210bfd595ff37306dbf5e
Amin Akkouche <aminakk42@gmail.com>
2022-11-16 20:05:35 +0100

    navBar changement

--- src/app/modules/acceuil/app.component.acceuil/navbar-vitrine/navbar-vitrine.component.ts
+++ src/app/modules/acceuil/app.component.acceuil/navbar-vitrine/navbar-vitrine.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 import { ConnectionComponent } from '../connection/connection.component';
-
+import { DOCUMENT } from '@angular/common'; 
 @Component({
   selector: 'app-navbar-vitrine',
   templateUrl: './navbar-vitrine.component.html',
@@ -10,11 +10,28 @@ import { ConnectionComponent } from '../connection/connection.component';
 export class NavbarVitrineComponent implements OnInit {
   @Output() public numPanel = new EventEmitter();
   constructor() {
+    
    }
 
   ngOnInit(): void {
-    
   }
+  
+ /* document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+          document.getElementById('navbar_top').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar_top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); */
+
+
   clicConnexion(){
   this.numPanel.emit(1);  
   
@@ -22,5 +39,7 @@ export class NavbarVitrineComponent implements OnInit {
   clicHome(){
     this.numPanel.emit(2);  
     
-    }
+  }
+  
 }
+
--- src/app/modules/acceuil/app.module.ts
+++ src/app/modules/acceuil/app.module.ts
@@ -5,7 +5,6 @@ import { AppModuleRoutingModule } from './app.module-routing';
 import { NavbarVitrineComponent } from './app.component.acceuil/navbar-vitrine/navbar-vitrine.component';
 import { ConnectionComponent } from './app.component.acceuil/connection/connection.component';
 import { HomeComponent } from './app.component.acceuil/home/home.component'
-
 @NgModule({
   declarations: [ComponentAcceuil, NavbarVitrineComponent, ConnectionComponent, HomeComponent],
   imports: [CommonModule, AppModuleRoutingModule],
