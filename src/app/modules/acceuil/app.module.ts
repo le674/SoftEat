867332d90f5800f2380afd175b542c2255038859
Amin Akkouche <aminakk42@gmail.com>
2022-11-16 23:36:34 +0100

    Responsive page presentation

--- src/app/modules/acceuil/app.module.ts
+++ src/app/modules/acceuil/app.module.ts
@@ -4,9 +4,10 @@ import { ComponentAcceuil } from './app.component.acceuil/app.component.acceuil'
 import { AppModuleRoutingModule } from './app.module-routing';
 import { NavbarVitrineComponent } from './app.component.acceuil/navbar-vitrine/navbar-vitrine.component';
 import { ConnectionComponent } from './app.component.acceuil/connection/connection.component';
-import { HomeComponent } from './app.component.acceuil/home/home.component'
+import { HomeComponent } from './app.component.acceuil/home/home.component';
+import { FonctionnaliteComponent } from './app.component.acceuil/fonctionnalite/fonctionnalite.component'
 @NgModule({
-  declarations: [ComponentAcceuil, NavbarVitrineComponent, ConnectionComponent, HomeComponent],
+  declarations: [ComponentAcceuil, NavbarVitrineComponent, ConnectionComponent, HomeComponent, FonctionnaliteComponent],
   imports: [CommonModule, AppModuleRoutingModule],
   exports: [],
   providers: [],
