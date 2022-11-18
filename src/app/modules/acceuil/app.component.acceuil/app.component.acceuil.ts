e613359beea9cd4f418aad60114c4e4e4adfa347
Amin Akkouche <aminakk42@gmail.com>
2022-11-18 18:30:12 +0100

    navBar+Connexion+FinTarif

--- src/app/modules/acceuil/app.component.acceuil/app.component.acceuil.ts
+++ src/app/modules/acceuil/app.component.acceuil/app.component.acceuil.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 })
 export class ComponentAcceuil implements OnInit {
-  public numeroPanel = -2;
+  public numeroPanel = 2;
   constructor() {
    }
 
--- src/app/modules/acceuil/app.module.ts
+++ src/app/modules/acceuil/app.module.ts
@@ -6,9 +6,11 @@ import { NavbarVitrineComponent } from './app.component.acceuil/navbar-vitrine/n
 import { ConnectionComponent } from './app.component.acceuil/connection/connection.component';
 import { HomeComponent } from './app.component.acceuil/home/home.component';
 import { FonctionnaliteComponent } from './app.component.acceuil/fonctionnalite/fonctionnalite.component';
-import { PrixComponent } from './app.component.acceuil/prix/prix.component'
+import { PrixComponent } from './app.component.acceuil/prix/prix.component';
+import { ProposComponent } from './app.component.acceuil/propos/propos.component';
+import { ContactComponent } from './app.component.acceuil/contact/contact.component'
 @NgModule({
-  declarations: [ComponentAcceuil, NavbarVitrineComponent, ConnectionComponent, HomeComponent, FonctionnaliteComponent, PrixComponent],
+  declarations: [ComponentAcceuil, NavbarVitrineComponent, ConnectionComponent, HomeComponent, FonctionnaliteComponent, PrixComponent, ProposComponent, ContactComponent],
   imports: [CommonModule, AppModuleRoutingModule],
   exports: [],
   providers: [],
