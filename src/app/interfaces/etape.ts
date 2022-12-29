e7356f115105a779c48e79af7d81e6dc88a76165
restomatic <restomatic@users.noreply.github.com>
2022-12-29 17:51:27 +0100

    Ajout de la class étape de préparation

--- src/app/interfaces/etape.ts
+++ src/app/interfaces/etape.ts
@@ -2,4 +2,36 @@ export interface Etape {
     "nom":string;
     "commentaire":string;
     "temps":number;
+
+    getNom():string;
+    setNom(nom:string):void;
+    getComentaire():string;
+    setComentaire(commentaire:string):void;
+    getTemps():number;
+    setTemps(temps:number):void;
+}
+
+export class Cetape implements Etape{
+    "nom": string;
+    "commentaire": string;
+    "temps": number;
+    getNom(): string {
+        return this.nom
+    }
+    setNom(nom: string): void {
+        this.nom = nom
+    }
+    getComentaire(): string {
+        return this.commentaire;
+    }
+    setComentaire(commentaire: string): void {
+       this.commentaire = commentaire
+    }
+    getTemps(): number {
+      return this.temps
+    }
+    setTemps(temps: number): void {
+        this.temps = temps
+    }
+
 }
