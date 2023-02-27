fe283a8b04d8d954f93090c4871fc0abbcba25e8
restomatic <restomatic@users.noreply.github.com>
2023-02-27 13:53:12 +0100

    amélioration du design de la page d'acceuil

--- src/environments/variables.ts
+++ src/environments/variables.ts
@@ -3,4 +3,4 @@ export const FIREBASE_STORAGE_EMULATOR_HOST="http://softeat-serveur:9199";
 export const FIREBASE_FUNCTIONS_EMULATOR_HOST="http://softeat-serveur:5001";
 export const FIREBASE_FIRESTORE_EMULATOR_HOST={host:"softeat-serveur", port:8080};
 export const FIREBASE_DATABASE_EMULATOR_HOST= {host:"softeat-serveur", port:9000};
-export const FIREBASE_PROD = false;
+export const FIREBASE_PROD = true;
