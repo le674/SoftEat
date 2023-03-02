bd68bff8bf0cedd1c6d0d60d89a5116160c6a009
restomatic <restomatic@users.noreply.github.com>
2023-03-02 19:42:51 +0100

    Dans le calcul de la valeur bouchère ajout de la possibilitée de rebaser celle-ci

--- src/app/services/menus/menu.calcul/menu.calcul.preparation/calcul.prepa.service.ts
+++ src/app/services/menus/menu.calcul/menu.calcul.preparation/calcul.prepa.service.ts
@@ -35,6 +35,7 @@ export class CalculPrepaService {
   async getPrimCost(prop:string,restaurant:string, etapes: Array<Cetape>, ingredients: Array<TIngredientBase>,
      consommables: Array<Cconsommable>){
     await this.restau_service.getSalaryCuisiniee(prop, restaurant).then((salary) => {
+
       let second_salary = 0;
       let full_time = 0;
       let full_cost_quant_ing:Array<number> = [0];
@@ -48,10 +49,12 @@ export class CalculPrepaService {
             return cost
           })
         }
+
       }
       if(consommables !== null){
         if(consommables.length > 0){
           full_cost_quant_conso = consommables.map((conso) => {
+            debugger;
             let cost =  conso.cost*conso.quantity;
             return cost
           })
@@ -112,26 +115,48 @@ export class CalculPrepaService {
 
   //  quand la quantitée finale est précise on applique le calcul normal de la valeur bouchère en revanche quand la quantitée finale 
   // est exprimé en pièce alors le coût pour une préparation
+  // on néglige les préparations qui ont une quantitée inférieure à 100g car alors ont divise le quotient
+  // par une puissance de 10 de sorte à avoir une valeur bouchère de l'ordre de grandeur du total des coûts 
+  // des ingrédients de base.
   getValBouchFromBasIng(base: TIngredientBase[], quantity_aft_prep: number, unity_aft_prep:string): number {
     let ingredient_quantity:Array<number> = [];
+    let square_final_cost = 0;
     if (base.length === 0) {
       return 0;
     }
-    const quantity_unity_act = this.calcul_service.convertQuantity(quantity_aft_prep, unity_aft_prep);
+
+    quantity_aft_prep = Number(quantity_aft_prep);
+    const quantity_unity_act = this.calcul_service.convertQuantity(Number(quantity_aft_prep), unity_aft_prep);
+    
     base.forEach((ingredient: TIngredientBase) => {
-     ingredient_quantity.push(this.calcul_service.convertQuantity(ingredient.quantity, ingredient.unity));
-    })
+      ingredient.quantity = Number(ingredient.quantity);
+      ingredient_quantity.push(this.calcul_service.convertQuantity(ingredient.quantity, ingredient.unity));
+    });
+    
     // on fait la somme des coûts et des quantitées des ingrédients de base utilisées pour la préparation
-    const moy_cost = base.map(ing => {
+    const total_cost = base.map(ing => {
       let cost =  ing.cost
+      ing.quantity_unity = Number(ing.quantity_unity);
+      ing.quantity = Number(ing.quantity);
       // on normalise le cout par la quantitée unitaire
       cost = cost*this.calcul_service.convertQuantity(ing.quantity, ing.unity)/this.calcul_service.convertQuantity(ing.quantity_unity, ing.unity);
       return cost
-    }).reduce((cost, next_cost) => cost + next_cost) / base.length;
-    const moy_total_cost = moy_cost;
-    const square_final_cost = quantity_unity_act * quantity_unity_act * ingredient_quantity.reduce((ing_prev, ing) => ing_prev + ing);
+    }).reduce((cost, next_cost) => cost + next_cost);
+    // si la quantitée après préparation est trop faible alors on recalcule le diviseur
+    if(quantity_unity_act * quantity_unity_act < 0.1){
+      let square_quant_act = quantity_unity_act * quantity_unity_act;
+      let curr = total_cost;
+      while(Math.round(curr) > 0) {
+        curr = curr/10;
+        square_quant_act = square_quant_act * 10;
+      }
+      square_final_cost = square_quant_act * ingredient_quantity.reduce((ing_prev, ing) => Number(ing_prev) + Number(ing));
+    }
+    else{
+     square_final_cost = quantity_unity_act * quantity_unity_act * ingredient_quantity.reduce((ing_prev, ing) => Number(ing_prev) + Number(ing));
+    }
     if (square_final_cost !== 0) {
-      return this.ToCentime(moy_total_cost / square_final_cost);
+      return this.ToCentime(total_cost / square_final_cost);
     }
     else {
       return 0
