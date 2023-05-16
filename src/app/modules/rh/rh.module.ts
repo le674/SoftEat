fea31e0f2c3968f33b53c14905e3d5538064018b
Valentin <valentin.pontiggia@telecom-st-etienne.fr>
2023-05-16 11:12:55 +0200

    added calendar access, view, and event creation

--- src/app/modules/rh/rh.module.ts
+++ src/app/modules/rh/rh.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { RhRoutingModule } from './rh-routing.module';
 import { AppRhComponent } from './app.rh/app.rh.component';
+import { CalendarComponent } from './app.rh/calendar/calendar.component';
 
 
 @NgModule({
   declarations: [
   
   
-    AppRhComponent
+    AppRhComponent,
+            CalendarComponent
   ],
   imports: [
     CommonModule,
