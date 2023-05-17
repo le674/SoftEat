391aeded9922c5033046771807bee82f7858a432
ROCA <benny13350@gmail.com>
2023-05-17 10:41:21 +0200

    première version du form évènement à planifier (~pas responsive~)

--- src/app/modules/rh/rh.module.ts
+++ src/app/modules/rh/rh.module.ts
@@ -5,13 +5,15 @@ import { RhRoutingModule } from './rh-routing.module';
 import { AppRhComponent } from './app.rh/app.rh.component';
 import { CalendarComponent } from './app.rh/calendar/calendar.component';
 import { HbarComponent } from './app.rh/hbar/hbar.component';
+import { EventFormComponent } from './app.rh/event-form/event-form.component';
 
 
 @NgModule({
   declarations: [
     AppRhComponent,
     CalendarComponent,
-    HbarComponent
+    HbarComponent,
+    EventFormComponent
   ],
   imports: [
     CommonModule,
