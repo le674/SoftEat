0b4be174d7eba0f68acd11df366fa32294b221f0
restomatic <restomatic@users.noreply.github.com>
2023-05-25 16:02:26 +0200

    Mise en place du backend pour l'envoie de sms via Amazon SNS pour les camapgnes

--- softeat_backend/mail.js
+++ softeat_backend/mail.js
@@ -1,15 +1,20 @@
 //FIREBASE_DATABASE_EMULATOR_HOST_1 = 'http://softeat-serveur:9000/?ns=psofteat-65478545498421319564'
 //NODE_ENV = "development"
+//Import du paquet pour la création de fonctions https
 const functions = require("firebase-functions");
+// import de firebase
 const admin = require("firebase-admin");
-const nodemailer = require("nodemailer");
 const serviceAccount = require('./firebase-conf/key.json');
-const cors = require("cors")({ origin: true });
 const app = admin.initializeApp({
     credential:  admin.credential.cert(serviceAccount), 
     databaseURL: "https://psofteat-65478545498421319564-default-rtdb.firebaseio.com"
-})
+}, "mail")
+// import de nodemailer
+const nodemailer = require("nodemailer");
+//import du paquet cors pour les problème connections cross origines
+const cors = require("cors")({ origin: true });
 /**
+ * 
 * Here we're using Gmail to send depuis 2022-05-02 on utilise une app password au lieu du mot de passe du compte
 */
 let transporter = nodemailer.createTransport({
@@ -19,8 +24,13 @@ let transporter = nodemailer.createTransport({
         pass: 'puhrqkirfohpmzfx'
     }
 });
-
-/**Permet d'envoyer des mail via nodeMailer  **/
+/**  
+ * Permet d'envoyer des mail via nodeMailer
+ * @param {string} req.query.from - personne qui envoie le message
+ * @param {string} req.query.subj- sujet du message envoyer
+ * @param {string} req.query.message - contenu du message envoyé
+ * @returns {void} ont transmet un mail au client
+*/
 exports.sendMail = functions.https.onRequest((req, res) => {
     res.setHeader("Access-Control-Allow-Origin","https://psofteat-65478545498421319564.web.app");
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
--- softeat_backend/sms.js
+++ softeat_backend/sms.js
@@ -1,33 +1,46 @@
+
+//Import du paquet pour la création de fonctions https
 const functions = require("firebase-functions");
+//Import de firebase
+const admin = require("firebase-admin");
+const serviceAccount = require('./firebase-conf/key.json');
+const app = admin.initializeApp({
+  credential:  admin.credential.cert(serviceAccount), 
+  databaseURL: "https://psofteat-65478545498421319564-default-rtdb.firebaseio.com"
+}, "sms")
+//Import de fs pour la lecture et le parsing de fichiers JSON + lecture et parsing du fichier de config AWS
 const fs = require('fs');
- const { SNSClient, CreateTopicCommand } = require("@aws-sdk/client-sns");
-const { fromIni } = require("@aws-sdk/credential-provider-ini");
-const credentials = fromIni({ profile: "default", configFilepath: "./aws-conf/credentials" });
 const configFile = fs.readFileSync("./aws-conf/config.json", 'utf-8');
 const config = JSON.parse(configFile);
-const serviceAccount = require('./firebase-conf/key.json');
-const cors = require("cors")({ origin: true });
-const app = admin.initializeApp({
-    credential:  admin.credential.cert(serviceAccount), 
-    databaseURL: "https://psofteat-65478545498421319564-default-rtdb.firebaseio.com"
-});
+//Import du Aws-sdk pour amazon SNS, lecture du fichier des accès, configuration d'un client sns 
+const { SNSClient, CreateTopicCommand, SubscribeCommand, PublishCommand} = require("@aws-sdk/client-sns");
+const { fromIni } = require("@aws-sdk/credential-provider-ini");
+const credentials = fromIni({ profile: "default", configFilepath: "./aws-conf/credentials" });
 const snsClient = new SNSClient({
     region: config.region,
     credentials: credentials,
 }); 
-/**Création d'un nouveau Topics pour l'envoie de SMS **/
+//Import de la librairie cors pour les requêtes Cross Origine
+const cors = require("cors")({ origin: true });
+/**  
+ * Création d'un nouveau Topics pour l'envoie de SMS
+ * @param {string} req.query.restaurant - restaurant auquel ont veut créer le topic
+ * @param {string} req.query.prop - nom de l'enseigne auquel nous créeons le topic
+ * @param {string} req.query.topic_name - nom du topic que l'on crée deux possibilités promotion et wast_prev(wast_prev correspond  aux alertes concernant le gaspisllage)    
+ * @returns {void} ont crée un sujet/topic
+*/
 exports.createTopic = functions.https.onRequest((req, res) => {   
     let defaultDatabase = app.database();
-    const ref_restaurants = defaultDatabase.ref(`restaurants_${enseigne}_${restaurant}/${enseigne}/${restaurant}`);
-    const ref_get_num_top = defaultDatabase.ref(`restaurants_${enseigne}_${restaurant}/${enseigne}/${restaurant}/num_topics`);
     const restaurant = req.query.restaurant;
     const prop = req.query.prop;
     const topic_name = req.query.topic_name;
+    const ref_restaurants = defaultDatabase.ref(`restaurants_${prop}_${restaurant}/${prop}/${restaurant}`);
+    const ref_get_num_top = defaultDatabase.ref(`restaurants_${prop}_${restaurant}/${prop}/${restaurant}/num_topics`);
     const id = `${prop}_${restaurant}_${topic_name}`;
-    const input = { // CreateTopicInput
-        Name: topic_name, // required
-        Tags: [ // TagList
-          { // Tag
+    const input = { 
+        Name: id, // required
+        Tags: [ 
+          {
             Key: id, // required
             Value: topic_name, // required
           },
@@ -48,19 +61,78 @@ exports.createTopic = functions.https.onRequest((req, res) => {
         snsClient.send(command).then(() => {
           ref_restaurants.update({
             [`num_topics`]: num_topic
+          }).then(() => {
+            res.status(200).send('Arrêt de la fonction : ajout d un topic et augmentation du nombre de topics maximal');
+            return;
           })
         })
+      }
+      else{
+        res.status(500).send('Nombre maximal de topic atteint aucun nouveau topics ajouté dans la base de donnée');
       }
     })
 });
 
 
-/**  Mise en place d'un abonnement des à un topic par les clients */
+/**  
+ * Mise en place d'un abonnement à un topic par les clients 
+ * @param {string} req.query.restaurant - restaurant auquel ont veut que le client souscrit
+ * @param {string} req.query.prop - nom de l'enseigne auquel le client souscrit
+ * @param {string} req.query.num - numéro du client qui souscrit
+ * @param {string} req.query.subject - sujet auquel le client souscrit deux topics existe, promotion et wast_prev(wast_prev correspond  aux alertes concernant le gaspisllage)    
+ * @returns {void} ont abonne le client au topic
+*/
 exports.subscribeClient = functions.https.onRequest((req, res) => {
-
+  const restaurant = req.query.restaurant;
+  const prop = req.query.prop;
+  const num = req.query.num;
+  const subject = req.query.subject;
+  const topic = prop + '_' + restaurant + '_' + subject;
+  const arn = 'arn:aws:sns:eu-west-3:395629972621:' + topic;
+  const input = { // SubscribeInput
+    TopicArn: arn, // required
+    Protocol: "sms", // required
+    Endpoint: num
+  };
+  const command = new SubscribeCommand(input);
+  return snsClient.send(command).then(() => {
+    res.status(200).send(`Une demmande d inscription au numéro ${num} vient de se réaliser`)
+  });
 });
 
-/** Envoie e message aux personnes abonnés à un topic */
+/**  
+ * Envoie un message aux personnes abonnés à un topic
+ * @param {string} req.query.restaurant - restaurant auquel ont veut que le client souscrit
+ * @param {string} req.query.prop - nom de l'enseigne auquel le client souscrit
+ * @param {string} req.query.msg - message que l'on envois aux personnes ayants souscrits au topic
+ * @param {string} req.query.subject - sujet auquel le client souscrit deux topics existe, promotion et wast_prev(wast_prev correspond  aux alertes concernant le gaspisllage)    
+ * @returns {void} ont envoie les messages aux clients
+*/
 exports.sendMessage = functions.https.onRequest((req, res) => {
-
+  let sub = "";
+  const client = new SNSClient(config);
+  const restaurant = req.query.restaurant;
+  const prop = req.query.prop;
+  const msg = req.query.msg;
+  const subject = req.query.subject;
+  const topic = prop + '_' + restaurant + '_' + subject;
+  const arn = 'arn:aws:sns:eu-west-3:395629972621:' + topic;
+  if(subject == "promotion"){
+    sub = "promotions"
+  }
+  if(subject == "wast_alertes_food"){
+    sub = "promotions déstockage"
+  }
+  const input = { // PublishInput
+    TopicArn: arn,
+    Message: msg, // required
+    Subject: `Information ${restaurant} : ${sub}`,
+    MessageStructure: "STRING_VALUE"
+  };
+  const command = new PublishCommand(input);
+  return client.send(command).then(() => {
+    res.status(200).send(`Nous venons d'envoyer une campagne de message pour le restaurant ${restaurant} de l'enseigne ${prop}`)
+  }).catch((e) => {
+    res.status(404).send(`Nous ne somme pas parvenus à envoyer de message pour le restaurant ${restaurant} de l'enseigne ${prop}`)
+  });
 });  
