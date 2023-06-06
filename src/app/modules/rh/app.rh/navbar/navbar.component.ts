2c72f905e425f5afe123b03c4f374d0de62e4b6e
Benoit <benoitauffret831@gmail.com>
2023-06-06 14:13:38 +0200

    add all + css

--- src/app/modules/rh/app.rh/navbar/navbar.component.ts
+++ src/app/modules/rh/app.rh/navbar/navbar.component.ts
@@ -10,10 +10,11 @@ export class NavbarComponent implements OnInit {
   Cuisiniers!: { nom: String; selectionne: boolean }[];
   Serveurs!: { nom: String; selectionne: boolean }[];
   Barmans!: { nom: String; selectionne: boolean }[];
-  Gerents!: { nom: String; selectionne: boolean }[];
+  Gerants!: { nom: String; selectionne: boolean }[];
 
   select!: String[];
-isChecked: any;
+  isChecked: any;
+  selectAll: boolean = false;
 
   constructor() {}
 
@@ -22,7 +23,7 @@ isChecked: any;
       { nom: 'Cuisiniers', open: false, buttonname: '▼' },
       { nom: 'Serveurs', open: false, buttonname: '▼' },
       { nom: 'Barmans', open: false, buttonname: '▼' },
-      { nom: 'Gérents', open: false, buttonname: '▼' },
+      { nom: 'Gérants', open: false, buttonname: '▼' },
     ];
     this.Cuisiniers = [
       { nom: 'Cuisto1', selectionne: false },
@@ -38,7 +39,7 @@ isChecked: any;
       { nom: 'Barman2', selectionne: false },
       { nom: 'Barman3', selectionne: false },
     ];
-    this.Gerents = [{ nom: 'Gerent1', selectionne: false }];
+    this.Gerants = [{ nom: 'Gerant1', selectionne: false }];
 
     this.select = [];
   }
@@ -52,23 +53,95 @@ isChecked: any;
     categories.open = !categories.open;
   }
 
-  /*selectEmployee(liste: any) {
-    if (!liste.selectionne) {
-      this.select.push(liste.nom);
-      liste.selectionne = !liste.selectionne;
-    } else {
-      this.select.splice(this.select.indexOf(liste.nom), 1);
-      liste.selectionne = !liste.selectionne;
-    }
-  }*/
+  /*
   selectEmployee(liste: any) {
     if (liste.selectionne) {
       this.select.splice(this.select.indexOf(liste.nom), 1);
     } else {
       this.select.push(liste.nom);
     }
-    
+
+    liste.selectionne = !liste.selectionne;
+  }
+  */
+
+  selectEmployee(liste: any) {  
+    if (liste.selectionne) {
+      const index = this.select.indexOf(liste.nom);
+      if (index !== -1) {
+        this.select.splice(index, 1);
+      }
+    } else {
+      this.select.push(liste.nom);
+    }
+  
     liste.selectionne = !liste.selectionne;
   }
   
+
+  addAllEmployees() {
+    if (this.selectAll) {
+      this.Cuisiniers.forEach((cuisto) => {
+        const index = this.select.indexOf(cuisto.nom);
+        if (index !== -1) {
+          this.select.splice(index, 1);
+          cuisto.selectionne = !cuisto.selectionne;
+        }
+      });
+  
+      this.Serveurs.forEach((serveur) => {
+        const index = this.select.indexOf(serveur.nom);
+        if (index !== -1) {
+          this.select.splice(index, 1);
+          serveur.selectionne = !serveur.selectionne;
+        }
+      });
+  
+      this.Barmans.forEach((barman) => {
+        const index = this.select.indexOf(barman.nom);
+        if (index !== -1) {
+          this.select.splice(index, 1);
+          barman.selectionne = !barman.selectionne;
+        }
+      });
+  
+      this.Gerants.forEach((gerant) => {
+        const index = this.select.indexOf(gerant.nom);
+        if (index !== -1) {
+          this.select.splice(index, 1);
+          gerant.selectionne = !gerant.selectionne;
+        }
+      });
+    } else {
+      this.Cuisiniers.forEach((cuisto) => {
+        if (!this.select.includes(cuisto.nom)) {
+          this.select.push(cuisto.nom);
+          cuisto.selectionne = !cuisto.selectionne;
+        }
+      });
+  
+      this.Serveurs.forEach((serveur) => {
+        if (!this.select.includes(serveur.nom)) {
+          this.select.push(serveur.nom);
+          serveur.selectionne = !serveur.selectionne;
+        }
+      });
+  
+      this.Barmans.forEach((barman) => {
+        if (!this.select.includes(barman.nom)) {
+          this.select.push(barman.nom);
+          barman.selectionne = !barman.selectionne;
+        }
+      });
+  
+      this.Gerants.forEach((gerant) => {
+        if (!this.select.includes(gerant.nom)) {
+          this.select.push(gerant.nom);
+          gerant.selectionne = !gerant.selectionne;
+        }
+      });
+    }
+  
+    this.selectAll = !this.selectAll;
+  }  
 }
