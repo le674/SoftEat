7ff8b69907df923e906f07483c0bf6a620ad5436
alban.lemiere@telecom-st-etienne.fr <alban.lemiere@telecom-st-etienne.fr>
2023-06-07 15:20:50 +0200

    suppressions car firebase.service

--- src/app/modules/messagerie/app.messagerie/app.messagerie.component.ts
+++ src/app/modules/messagerie/app.messagerie/app.messagerie.component.ts
@@ -1,4 +1,6 @@
 import { Component, NgModule, OnInit } from '@angular/core';
+import { FirebaseService } from '../../../services/firebase.service';
+import { Statut } from '../../../interfaces/statut';
 
 @Component({
   selector: 'app-messagerie',
@@ -10,12 +12,17 @@ import { Component, NgModule, OnInit } from '@angular/core';
 export class AppMessagerieComponent implements OnInit {
   text!: string;
   notification!: boolean[];
+  private db: any;
+  statut!: Statut;
+  userId = '0uNzmnBI0jYYspF4wNXdRd2xw9Q2'; //  ID de l'utilisateur à récupérer
 
-  constructor() { }
+  constructor(private firebaseService: FirebaseService) { }
 
   ngOnInit(): void {
     this.text = "it works !";
     this.notification = [true, true, true, true, true, true, true];
+    // this.db = this.firebaseService.getDatabaseInstance();
+    this.statut = this.firebaseService.fetchUserStatus(this.userId);
   }
 
   date: Date = new Date();
@@ -24,7 +31,8 @@ export class AppMessagerieComponent implements OnInit {
   budget = true;
   facture = true;
   planning = true;
-  stock = true;
+  stock = (this.statut.stock === 'rw');
+  
 
   updateNotification(index: number){
     this.notification[index] = !this.notification[index];
