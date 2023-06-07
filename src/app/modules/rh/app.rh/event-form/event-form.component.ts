646774d936206ba22e5512e06d4be93cdf1e5a97
ROCA <benny13350@gmail.com>
2023-06-07 11:52:07 +0200

    event form plus clean et responsive -> manque la tête du bouton save

--- src/app/modules/rh/app.rh/event-form/event-form.component.ts
+++ src/app/modules/rh/app.rh/event-form/event-form.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-event-form',
   templateUrl: './event-form.component.html',
   styleUrls: ['./event-form.component.css']
 })
 
-export class EventFormComponent implements OnInit {
+export class EventFormComponent implements OnInit, AfterViewInit {
   @ViewChild('addPersonnel') addPersonnelInput!: ElementRef<HTMLInputElement>;
   @ViewChild('addMotif') addMotifInput!: ElementRef<HTMLInputElement>;
   @ViewChild('addEvent') addEventInput!: ElementRef<HTMLInputElement>;
@@ -13,11 +13,39 @@ export class EventFormComponent implements OnInit {
   @ViewChild('addPrisePoste') addPrisePosteInput!: ElementRef<HTMLInputElement>;
   @ViewChild('addFinPoste') addFinPosteInput!: ElementRef<HTMLInputElement>;
   @ViewChild('addRepeter') addRepeterSelect!: ElementRef<HTMLSelectElement>;
+  dateWidth = '150px'; // Default width
   constructor() { }
 
+  /* Les deux premières méthodes ajoutent automatiquement des majuscules sur 
+  le premier caractère des champs Personnel, Evenement et Lieu pour uniformiser */
   ngOnInit(): void {
+    const inputFields: HTMLInputElement[] = [
+      this.addPersonnelInput.nativeElement,
+      this.addEventInput.nativeElement,
+      this.addLieuInput.nativeElement,
+    ];
+  
+    inputFields.forEach((input) => {
+      input.addEventListener('input', () => {
+        this.truncateInputValue(input);
+      });
+    });
   }
 
+  truncateInputValue(input: HTMLInputElement): void {
+    input.value = input.value.charAt(0).toUpperCase() + input.value.slice(1);
+  }
+
+  /* Les 2 méthodes suivantes permettent de rendre l'espace occupé par la date responsive*/ 
+  ngAfterViewInit(): void {
+    this.calculateInputWidth();
+  }
+
+  calculateInputWidth(): void {
+    const inputElement = this.addFinPosteInput.nativeElement;
+    const contentWidth = inputElement.scrollWidth + 'px';
+    this.dateWidth = contentWidth;
+  }
 
   rows: Row[] = [];
 
@@ -48,7 +76,7 @@ export class EventFormComponent implements OnInit {
       !this.isFieldFilled(this.addFinPosteInput) ||
       !this.isFieldFilled(this.addRepeterSelect)) {
       // Show the popup or perform any required validation logic
-      alert("Renseignez les champs obligatoires : Personnel, Lieu, Prise et Fin de Poste, Répéter l'évènement.");
+      alert("Renseignez les champs obligatoires marqués par un astérisque (*)");
       return;
     }
     this.addRow(personnel, motif, event, lieu, prisePoste, finPoste, repeter);
