b8e2d048e2e82be79beac6b43cc56bcc45e329fc
dauriat.clement <clement.dauriat@etu.univ-st-etienne.fr>
2023-06-07 16:42:19 +0200

    try to send messages

--- src/app/modules/messagerie/app.messagerie/app.messagerie.component.ts
+++ src/app/modules/messagerie/app.messagerie/app.messagerie.component.ts
@@ -1,6 +1,8 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { FirebaseService } from '../../../services/firebase.service';
 import { Statut } from '../../../interfaces/statut';
+import { FirebaseApp, initializeApp } from "@angular/fire/app";
+import { getDatabase, ref, push, set, update} from 'firebase/database';
 
 @Component({
   selector: 'app-messagerie',
@@ -44,12 +46,41 @@ export class AppMessagerieComponent implements OnInit {
   }
   
 
-  
+    messageInput = document.getElementById("input");
+
 
   updateNotification(index: number){
     this.notification[index] = !this.notification[index];
   }
   
+  sendMessage(message: string){
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyDPJyOCyUMDl70InJyJLwNLAwfiYnrtsDo",
+      authDomain: "psofteat-65478545498421319564.firebaseapp.com",
+      databaseURL: "https://psofteat-65478545498421319564-default-rtdb.firebaseio.com",
+      projectId: "psofteat-65478545498421319564",
+      storageBucket: "psofteat-65478545498421319564.appspot.com",
+      messagingSenderId: "135059251548",
+      appId: "1:135059251548:web:fb05e45e1d1631953f6199",
+      measurementId: "G-5FBJE9WH0X"
+    };
+
+    const firebaseApp = initializeApp(firebaseConfig);
+    const db = getDatabase(firebaseApp);
+
+    if(message!=''){
+        // Ajoutez le message à la base de données Firebase
+        update(ref(db), {
+          ["conversations/tel42_ana_573902"]:{
+            auteur: 'matthieu',
+            content: message,
+            horodatage: Date.now()
+          }
+        });
+    }
+    
+  }
 
 }
 
