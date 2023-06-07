c5c4b9fa6bed4214b6af17655d0f231461db46cf
Matthieu d'Hoop <matthieu.dhoop@gmail.com>
2023-06-07 11:46:55 +0200

    method to get user infos

--- src/app/modules/messagerie/app.message.template/app.message.template.component.ts
+++ src/app/modules/messagerie/app.message.template/app.message.template.component.ts
@@ -100,6 +100,23 @@ export class AppMessageTemplateComponent implements OnInit {
     this.separationDateB = !this.separationDateB;
   }
 
+  fetchUserInformations() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDPJyOCyUMDl70InJyJLwNLAwfiYnrtsDo",
+      authDomain: "psofteat-65478545498421319564.firebaseapp.com",
+      databaseURL: "https://psofteat-65478545498421319564-default-rtdb.firebaseio.com",
+      projectId: "psofteat-65478545498421319564",
+      storageBucket: "psofteat-65478545498421319564.appspot.com",
+      messagingSenderId: "135059251548",
+      appId: "1:135059251548:web:fb05e45e1d1631953f6199",
+      measurementId: "G-5FBJE9WH0X"
+    };
+    const firebaseApp = initializeApp(firebaseConfig);
+    const db = getDatabase(firebaseApp);
+
+    
+  }
+
   fetchUserStatus() {
     const userId = '0uNzmnBI0jYYspF4wNXdRd2xw9Q2'; //  ID de l'utilisateur à récupérer
 
@@ -121,11 +138,14 @@ export class AppMessageTemplateComponent implements OnInit {
       console.log('Une erreur s\'est produite lors de la récupération des statuts :', error);
     });
 
-    this.text = "Voici mes statuts :\n alertes : " + this.statut.alertes 
-    + ",\n analyse : " + this.statut.analyse + ",\n budget : " 
-    + this.statut.budget + ",\n facture : " + this.statut.facture 
-    + ",\n planning : " + this.statut.planning + ",\n stock : " 
-    + this.statut.stock + ".";
+    // this.text = "Voici mes statuts :\n alertes : " + this.statut.alertes 
+    // + ",\n analyse : " + this.statut.analyse + ",\n budget : " 
+    // + this.statut.budget + ",\n facture : " + this.statut.facture 
+    // + ",\n planning : " + this.statut.planning + ",\n stock : " 
+    // + this.statut.stock + ".";
+    console.log(db);
+    // const userConversations = ref(db, 'restaurants/' + )
+    this.text = "hey";
 
 
 
