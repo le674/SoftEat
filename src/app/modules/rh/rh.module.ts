f6eb14de141e1991274a632d04403a6be22b919f
Valentin <valentin.pontiggia@telecom-st-etienne.fr>
2023-06-07 11:24:55 +0200

    added scheduler and calendar view

--- src/app/modules/rh/rh.module.ts
+++ src/app/modules/rh/rh.module.ts
@@ -1,26 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SchedulerDataService } from './app.rh/calendar/scheduler-data.service';
+import { CalendarDataService } from './calendar-view/calendar-data.service';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { CalendarComponent } from './app.rh/calendar/calendar.component';
+import { DayPilotModule } from 'daypilot-pro-angular';
+import { HttpClientModule } from '@angular/common/http';
 import { RhRoutingModule } from './rh-routing.module';
 import { AppRhComponent } from './app.rh/app.rh.component';
-import { CalendarComponent } from './app.rh/calendar/calendar.component';
 import { HbarComponent } from './app.rh/hbar/hbar.component';
-
+import { CalendarViewComponent } from './calendar-view/calendar-view.component';
 
 @NgModule({
   declarations: [
-  
-  
     AppRhComponent,
-            CalendarComponent,
-            HbarComponent
+    CalendarComponent,
+    HbarComponent,
+    CalendarViewComponent
   ],
   imports: [
     CommonModule,
-    RhRoutingModule
+    RhRoutingModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    DayPilotModule
   ],
   exports: [
-    AppRhComponent
+    AppRhComponent,
+    CalendarComponent,
+    CalendarViewComponent
+  ],
+  providers: [
+    SchedulerDataService,
+    CalendarDataService
   ]
 })
 export class RhModule { }
