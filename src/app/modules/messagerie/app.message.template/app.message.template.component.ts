4834f3b5a3502f36d1b0d73b35ba3cc1cce83346
alban.lemiere@telecom-st-etienne.fr <alban.lemiere@telecom-st-etienne.fr>
2023-06-07 15:20:50 +0200

    suppressions car firebase.service

--- src/app/modules/messagerie/app.message.template/app.message.template.component.ts
+++ src/app/modules/messagerie/app.message.template/app.message.template.component.ts
@@ -3,7 +3,7 @@ import { FirebaseApp, initializeApp } from "@angular/fire/app";
 import { getDatabase, ref, onValue} from 'firebase/database';
 import { Statut } from '../../../interfaces/statut';
 import { HttpClient } from '@angular/common/http';
-
+import { FirebaseService } from '../../../services/firebase.service';
 
 
 @Component({
@@ -20,13 +20,11 @@ export class AppMessageTemplateComponent implements OnInit {
   email!: string;
   // private http!: HttpClient; // Dois être défini dans le constructeur
   heure!: string;
+  private db: any;
   
 
 
-
-
-
-  constructor(private http: HttpClient) { }
+  constructor(private firebaseService: FirebaseService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.message = "received";
@@ -34,6 +32,7 @@ export class AppMessageTemplateComponent implements OnInit {
     this.separationDateB = true;
     this.statut = {is_prop:false, stock:"", alertes:"", analyse:"", budget:"", facture:"", planning:""};
     this.email = "";
+    this.db = this.firebaseService.getDatabaseInstance();
     this.fetchUserStatus();
     this.fetchTimeServer();
     
@@ -46,29 +45,15 @@ export class AppMessageTemplateComponent implements OnInit {
   fetchUserStatus() {
     const userId = '0uNzmnBI0jYYspF4wNXdRd2xw9Q2'; //  ID de l'utilisateur à récupérer
 
-    const firebaseConfig = {
-      apiKey: "AIzaSyDPJyOCyUMDl70InJyJLwNLAwfiYnrtsDo",
-      authDomain: "psofteat-65478545498421319564.firebaseapp.com",
-      databaseURL: "https://psofteat-65478545498421319564-default-rtdb.firebaseio.com",
-      projectId: "psofteat-65478545498421319564",
-      storageBucket: "psofteat-65478545498421319564.appspot.com",
-      messagingSenderId: "135059251548",
-      appId: "1:135059251548:web:fb05e45e1d1631953f6199",
-      measurementId: "G-5FBJE9WH0X"
-    };
-    const firebaseApp = initializeApp(firebaseConfig);
-    const db = getDatabase(firebaseApp);
-    
-    console.log(db);
-    const userEmailRef = ref(db, 'users/foodandboost_prop/' + userId + '/email');
-    const userStatusRef = ref(db, 'users/foodandboost_prop/' + userId + '/statut');
+    const userEmailRef = ref(this.db, 'users/foodandboost_prop/' + userId + '/email');
+    const userStatusRef = ref(this.db, 'users/foodandboost_prop/' + userId + '/statut');
 
     onValue(userEmailRef, (snapshot) => {
       this.email = snapshot.val();
     });
     onValue(userStatusRef, (snapshot) => {
       const statut = snapshot.val();
-      this.statut.alertes = statut.alertes;
+      // this.statut.alertes = statut.alertes;
       this.statut.analyse = statut.analyse;
       this.statut.budget = statut.budget;
       this.statut.facture = statut.facture;
@@ -78,9 +63,8 @@ export class AppMessageTemplateComponent implements OnInit {
       console.log('Une erreur s\'est produite lors de la récupération des statuts :', error);
     });
 
-    this.text = "Voici mes statuts :\n alertes : " + this.statut.alertes + ",\n analyse : " + this.statut.analyse + ",\n budget : " + this.statut.budget + ",\n facture : " + this.statut.facture + ",\n planning : " + this.statut.planning + ",\n stock : " + this.statut.stock + ".";
+    this.text = "Voici mes statuts :\n analyse : " + this.statut.analyse + ",\n budget : " + this.statut.budget + ",\n facture : " + this.statut.facture + ",\n planning : " + this.statut.planning + ",\n stock : " + this.statut.stock + ".";
 
-    
 
   }
 
