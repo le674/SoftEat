588f2fb56539673add85502b91466da663a1f8f5
Matthieu d'Hoop <matthieu.dhoop@gmail.com>
2023-06-08 09:49:18 +0200

    get user email

--- src/app/modules/messagerie/app.message.template/app.message.template.component.ts
+++ src/app/modules/messagerie/app.message.template/app.message.template.component.ts
@@ -68,10 +68,6 @@ export class AppMessageTemplateComponent implements OnInit {
     // + this.statut.stock + ".";
     // const userConversations = ref(db, 'restaurants/' + )
     this.text = localStorage.getItem("user_email") as string;
-
-
-
-
   }
 
   fetchTimeServer(){
@@ -87,53 +83,6 @@ export class AppMessageTemplateComponent implements OnInit {
         hours -= 24;
       };
       this.heure = hours.toString().padStart(2, '0') + ':' + utcHourSplit[1];
-    });
-  }
-
-  updateSeparationDate(){
-    this.separationDateB = !this.separationDateB;
-  }
-
-  fetchUserStatus() {
-    const userId = '0uNzmnBI0jYYspF4wNXdRd2xw9Q2'; //  ID de l'utilisateur à récupérer
-
-    const userEmailRef = ref(this.db, 'users/foodandboost_prop/' + userId + '/email');
-    const userStatusRef = ref(this.db, 'users/foodandboost_prop/' + userId + '/statut');
-
-    onValue(userEmailRef, (snapshot) => {
-      this.email = snapshot.val();
-    });
-    onValue(userStatusRef, (snapshot) => {
-      const statut = snapshot.val();
-      // this.statut.alertes = statut.alertes;
-      this.statut.analyse = statut.analyse;
-      this.statut.budget = statut.budget;
-      this.statut.facture = statut.facture;
-      this.statut.planning = statut.planning;
-      this.statut.stock = statut.stock;
-    }, (error) => {
-      console.log('Une erreur s\'est produite lors de la récupération des statuts :', error);
-    });
-
-    this.text = "Voici mes statuts :\n analyse : " + this.statut.analyse + ",\n budget : " + this.statut.budget + ",\n facture : " + this.statut.facture + ",\n planning : " + this.statut.planning + ",\n stock : " + this.statut.stock + ".";
-
-
-  }
-
-  fetchTimeServer(){
-    this.http.get('http://worldtimeapi.org/api/timezone/Europe/Paris').subscribe((data: any) => {
-      const utcDateTime = data.utc_datetime.slice(11,16); //"utc_datetime": "2023-06-06T12:50:44.493419+00:00"
-      const utcOffset = data.utc_offset; //"utc_offset": "+02:00"
-      const offsetHours = parseInt(utcOffset.slice(1, 3), 10);
-      const utcHourSplit = utcDateTime.split(':');
-      
-      const hoursInt = parseInt(utcHourSplit[0], 10);
-      let hours = hoursInt + offsetHours;
-      if (hours >= 24) {
-        hours -= 24;
-      };
-      this.heure = hours.toString().padStart(2, '0') + ':' + utcHourSplit[1];
-
     });
   }
 }
