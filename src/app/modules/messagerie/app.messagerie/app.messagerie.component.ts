52e385683e326f845d28fd063d6771cfdd45e0f7
Matthieu d'Hoop <matthieu.dhoop@gmail.com>
2023-06-09 15:03:49 +0200

    sending message successful

--- src/app/modules/messagerie/app.messagerie/app.messagerie.component.ts
+++ src/app/modules/messagerie/app.messagerie/app.messagerie.component.ts
@@ -60,33 +60,17 @@ export class AppMessagerieComponent implements OnInit {
     if(this.inputText != '') {
       const db = getDatabase(this.firebaseApp);
 
-      //Récupération du nombre de messages dans la conversation
-      const nb_messageRef = ref(db, "conversations/deliss_pizz/deliss_pizz/del42_ana_037581");
-      let nb_messages = 0;
-      onValue(nb_messageRef, (snapshot) => {
-        nb_messages = snapshot.val();
-        console.log("Retrieved data:", nb_messages);
-      }, (error) => {
-        console.error("Error retrieving data:", error);
-      });
-
       //Création du nouveau message
       const newMessage = {
         auteur: 'matthieu',
         contenu: this.inputText,
-        horodatage: new Date().getHours() + ":" + new Date().getMinutes()
+        horodatage: new Date().getTime()
       }
 
       //Ecriture du message dans la BDD
-      const nodeName = `message_${nb_messages}`;
       const nodeRef = ref(db, `conversations/deliss_pizz/deliss_pizz/del42_ana_037581`);
       
-      set(nodeRef, newMessage)
-      //Mise à jour du nombre de messages
-
-      set(ref(db, "conversations/deliss_pizz/deliss_pizz/del42_ana_037581"), {
-        nb_messages: nb_messages+1
-      }).then(() => {
+      push(nodeRef, newMessage).then(() => {
         console.log("New message with custom name created successfully");
       })
       .catch((error) => {
