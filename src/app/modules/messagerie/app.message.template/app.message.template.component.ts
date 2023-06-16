11093438e35779e7b90eda3e944bace4436d844f
Matthieu d'Hoop <matthieu.dhoop@gmail.com>
2023-06-16 16:15:33 +0200

    message à gauche et à droite  selon l'auteur

--- src/app/modules/messagerie/app.message.template/app.message.template.component.ts
+++ src/app/modules/messagerie/app.message.template/app.message.template.component.ts
@@ -1,9 +1,6 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
-import { FirebaseApp } from "@angular/fire/app";
-import { getDatabase, ref, onValue, get} from 'firebase/database';
+import { Component, Input, OnInit } from '@angular/core';
 import { Statut } from '../../../interfaces/statut';
 import { MessageModel } from '../messages_models/model';
-import { DatePipe } from '@angular/common';
 @Component({
   selector: 'message-template',
   templateUrl: './app.message.template.component.html',
@@ -13,20 +10,16 @@ import { DatePipe } from '@angular/common';
 export class AppMessageTemplateComponent implements OnInit {
 
   @Input() listeMessages!: MessageModel;
+  @Input() author_is_me!: boolean;
+  @Input() isBot!: boolean;
 
-
-  message!: string;
-  email!: any;
-  firebaseApp: FirebaseApp | undefined;
-
+  statut!: Statut;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.message = "received";
+    this.statut = {is_prop:false, stock:"", alertes:"", analyse:"", budget:"", facture:"", planning:""};
   }
-
-
 }
 
 
