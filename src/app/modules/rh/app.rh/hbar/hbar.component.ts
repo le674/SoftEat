7aae639268b166ba91465da3767f7c4ea0c4ddf1
ROCA <benny13350@gmail.com>
2023-06-19 12:07:16 +0200

    j ai reussi à envoyer un message (enregistrement bdd)

--- src/app/modules/rh/app.rh/hbar/hbar.component.ts
+++ src/app/modules/rh/app.rh/hbar/hbar.component.ts
@@ -1,15 +1,15 @@
-import { Component, ElementRef, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ChangeDetectorRef, Input, Inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import {AppRhComponent} from '../app.rh.component'
-import { getDatabase, ref, push, update, get, onChildAdded, onValue, DatabaseReference} from 'firebase/database';
-
+import { AppRhComponent } from '../app.rh.component'
+import { getDatabase, ref, push, update, get, onChildAdded, onValue, DatabaseReference } from 'firebase/database';
+import { FirebaseApp } from '@angular/fire/app';
 @Component({
   selector: 'app-hbar',
   templateUrl: './hbar.component.html',
   styleUrls: ['./hbar.component.css']
 })
 export class HbarComponent implements OnInit {
-  @ViewChild('form') form!: NgForm; 
+  @ViewChild('form') form!: NgForm;
   @ViewChild('motif') motif!: ElementRef;
   @ViewChild('autofillConge') autofillConge!: ElementRef;
   @ViewChild('autofillRTT') autofillRTT!: ElementRef;
@@ -23,11 +23,16 @@ export class HbarComponent implements OnInit {
   selectedShortenedFileName!: string;
   selectedFileName!: string;
   // Pour la demande de congés envoyée
+  @Input() convActive: string = 'conversations/deliss_pizz/employes/telecom3_trois_telecom'; // Propriété d'entrée pour convActive
+  firebaseApp: FirebaseApp | undefined;
   date!: number;
 
-  constructor(private cdr: ChangeDetectorRef, private app: AppRhComponent) { }
+  constructor(private cdr: ChangeDetectorRef, private app: AppRhComponent, firebaseApp: FirebaseApp
+  ) {
+    this.firebaseApp = firebaseApp;
+  }
 
-  /* Les 2 méthodes suivantes permettent de rendre l'espace occupé par la date adapté*/ 
+  /* Les 2 méthodes suivantes permettent de rendre l'espace occupé par la date adapté*/
   ngAfterViewInit(): void {
     this.calculateInputWidth();
     this.cdr.detectChanges();
@@ -51,7 +56,7 @@ export class HbarComponent implements OnInit {
     const green = Math.round(normalizedValue * 255);
     return { color: `rgb(${red}, ${green}, 0)` };
   }
-  
+
   /*autofillInput(value: string): void {
     if (value =="Exceptionnels"){
       this.motif.nativeElement.value = '';
@@ -65,7 +70,7 @@ export class HbarComponent implements OnInit {
       this.form.value.motif = value;
     }
   }
-  
+
 
   displayFileName(event: any) {
     const fileInput = event.target;
@@ -78,8 +83,8 @@ export class HbarComponent implements OnInit {
       this.selectedShortenedFileName = '';
     }
   }
-  
-  
+
+
   getShortenedFileName(fileName: string): string {
     if (fileName.length <= 20) {
       return fileName;
@@ -87,7 +92,7 @@ export class HbarComponent implements OnInit {
       return fileName.substr(0, 17) + '...';
     }
   }
-  
+
   unchooseFile() {
     this.selectedFileName = '';
     // Reset the file input value if needed
@@ -99,17 +104,16 @@ export class HbarComponent implements OnInit {
     if (this.form.valid) { // Check if the form is valid
       const { motif, dateDebut, dateFin } = this.form.value;
       let message = `Motif: ${motif}\nDate début: ${dateDebut}\nDate fin: ${dateFin}`;
-  
+
       if (this.selectedFileName) {
         message += `\nFichier joint: ${this.selectedFileName}`;
       }
-      this.date = this.fetchTimeServer();
       message += `\nHeure du mess: ${this.date}`;
-  
-      alert(message);
+      console.log('je vais ajouter un message');
+      this.sendMessage(message);
     }
   }
-  
+
 
   //recuperation heure du serveur
   fetchTimeServer(): number {
@@ -121,31 +125,20 @@ export class HbarComponent implements OnInit {
     return this.date;
   }
 
-  /*async sendMessage(): Promise<void> {
-    if(this.inputText != '') {
-      const db = getDatabase(this.firebaseApp);
+  async sendMessage(message: string): Promise<void> {
+    const db = getDatabase(this.firebaseApp);
 
-      //Création du nouveau message
-      const newMessage = {
-        auteur: localStorage.getItem("user_email"),
-        contenu: this.inputText,
-        horodatage: this.fetchTimeServer(),
-        nom : this.name,
-        prenom : this.surname
-      }
-      //Ecriture du message dans la BDD
-      const nodeRef = ref(db, this.convActive);
-      push(nodeRef, newMessage).then(() => {
-        //Envoie de la notification à tous les Users
-        this.updateUnreadMessages(this.canalActiveId, this.convListUsers[this.canalActiveId]);
-        this.markCanalAsRead(this.canalActiveId, this.email);
-      })
-      .catch((error) => {
-        console.error("Error creating new message:", error);
-      });
-      
-      
+    //Création du nouveau message
+    const newMessage = {
+      auteur: localStorage.getItem("user_email"),
+      contenu: message,
+      horodatage: this.fetchTimeServer(),
+      //nom: this.name,
+      //prenom: this.surname
     }
-    this.inputText = "";
-  }*/
+    //Ecriture du message dans la BDD
+    const nodeRef = ref(db, this.convActive);
+    push(nodeRef, newMessage);
+    console.log('jai du ajouter un message askip')
+  }
 }
